0032f0e4ebe5d8a0c132580b84a42cfbe69d5958
kim gwanhee <rhksgml55@gmail.com>
2026-04-19 23:13:04 +0900

    refactor: BinaryContentService Entity 직접 노출 제거

--- src/main/java/com/sprint/mission/discodeit/service/BinaryContentService.java
+++ src/main/java/com/sprint/mission/discodeit/service/BinaryContentService.java
@@ -1,18 +1,18 @@
 package com.sprint.mission.discodeit.service;
 
+import com.sprint.mission.discodeit.dto.data.BinaryContentDto;
 import com.sprint.mission.discodeit.dto.request.BinaryContentCreateRequest;
-import com.sprint.mission.discodeit.entity.BinaryContent;
 
 import java.util.List;
 import java.util.UUID;
 
 public interface BinaryContentService {
 
-  BinaryContent create(BinaryContentCreateRequest request);
+  BinaryContentDto create(BinaryContentCreateRequest request);
 
-  BinaryContent find(UUID binaryContentId);
+  BinaryContentDto find(UUID binaryContentId);
 
-  List<BinaryContent> findAllByIdIn(List<UUID> ids);
+  List<BinaryContentDto> findAllByIdIn(List<UUID> ids);
 
   void delete(UUID binaryContentId);
 }
--- src/main/java/com/sprint/mission/discodeit/service/basic/BasicBinaryContentService.java
+++ src/main/java/com/sprint/mission/discodeit/service/basic/BasicBinaryContentService.java
@@ -1,7 +1,9 @@
 package com.sprint.mission.discodeit.service.basic;
 
+import com.sprint.mission.discodeit.dto.data.BinaryContentDto;
 import com.sprint.mission.discodeit.dto.request.BinaryContentCreateRequest;
 import com.sprint.mission.discodeit.entity.BinaryContent;
+import com.sprint.mission.discodeit.mapper.BinaryContentMapper;
 import com.sprint.mission.discodeit.repository.BinaryContentRepository;
 import com.sprint.mission.discodeit.service.BinaryContentService;
 import com.sprint.mission.discodeit.storage.BinaryContentStorage;
@@ -18,9 +20,10 @@ public class BasicBinaryContentService implements BinaryContentService {
 
   private final BinaryContentRepository binaryContentRepository;
   private final BinaryContentStorage binaryContentStorage;
+  private final BinaryContentMapper binaryContentMapper;
 
   @Override
-  public BinaryContent create(BinaryContentCreateRequest request) {
+  public BinaryContentDto create(BinaryContentCreateRequest request) {
     String fileName = request.fileName();
     byte[] bytes = request.bytes();
     String contentType = request.contentType();
@@ -34,19 +37,22 @@ public class BasicBinaryContentService implements BinaryContentService {
     BinaryContent savedBinaryContent = binaryContentRepository.save(binaryContent);
     binaryContentStorage.put(savedBinaryContent.getId(), bytes);
 
-    return savedBinaryContent;
+    return binaryContentMapper.toDto(savedBinaryContent);
   }
 
   @Override
-  public BinaryContent find(UUID binaryContentId) {
+  public BinaryContentDto find(UUID binaryContentId) {
     return binaryContentRepository.findById(binaryContentId)
+            .map(binaryContentMapper::toDto)
             .orElseThrow(() ->
                     new NoSuchElementException("BinaryContent with id " + binaryContentId + " not found"));
   }
 
   @Override
-  public List<BinaryContent> findAllByIdIn(List<UUID> ids) {
-    return binaryContentRepository.findAllByIdIn(ids);
+  public List<BinaryContentDto> findAllByIdIn(List<UUID> ids) {
+    return binaryContentRepository.findAllByIdIn(ids).stream()
+            .map(binaryContentMapper::toDto)
+            .toList();
   }
 
   @Override
